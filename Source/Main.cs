db577e7eac667ad2a4b099a9d70919e0daa0f4f6
InsertSampleText <87501842+InsertSampleText@users.noreply.github.com>
2025-11-02 23:49:08 -0500

    update
    added more psycasts
    added more folders
    added more art

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,11 +1,36 @@
-using Rimworld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
+using Verse.Sound;
+using Verse.AI.Group;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
 
 
 namespace ExtraPsycasts
 {
-    
 
+    //Watering pinhole
+    public class CompProperties_EP_FertilityBoost : CompProperties
+    {
+
+        public float fertilityBoost;
+
+        public CompProperties_EP_FertilityBoost()
+        {
+            compClass = typeof(Comp_EP_FertilityBoost);
+        }
+
+    }
+    
+    public class Comp_EP_FertilityBoost : ThingComp
+    {
+        
+        
+
+    }
 
 
     
